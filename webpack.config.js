f10aac298080b9e8b1188b051a4a36afb4c85d7e
刘健 <liujian@ydjdev.com>
2023-03-04 08:23:02 +0800

    feat: 开启 webpack 打包编译监听，有文件修改就会再次触发构建

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
+    watch: true,
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'dist'),
