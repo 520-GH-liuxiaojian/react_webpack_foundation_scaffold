29ab5cc1d310dd581cf8721b1b0b353a1693ca9c
刘健 <liujian@ydjdev.com>
2023-03-04 08:24:52 +0800

    feat: 监听缺点说明 &  监听配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,16 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    watch: true,
+    watch: true, // 每次更新之后，需要手动刷新浏览器，没有办法实现文件更新之后，浏览器自动刷新
+    watchOptions: {
+        // 默认是空，不监听文件或者文件夹，支持正则匹配
+        // 不监听 node_modules 文件夹，性能会提升
+        ignored: /node_modules/,
+        // 监听到变化过后会等待 300ms 再去执行，默认 300ms
+        aggregateTimeout: 300,
+        // 判断文件是否发生变化，是通过不停的询问西系统指定的文件有没有变化实现的，默认是每秒 1000 次
+        poll: 1000
+    },
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'dist'),
